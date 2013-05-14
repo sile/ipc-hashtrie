1c6618ea6144d271dddc53ab32d013258dd632d8
Takeru Ohta <phjgt308@gmail.com>
2013-05-14 11:00:18 +0900

    mutex用のベンチマークを汎用コマンドに移動

--- src/bin/map.hh
+++ src/bin/map.hh
@@ -1,8 +1,90 @@
 #ifndef __MAP_HH__
 #define __MAP_HH__
 
+#include <string>
+#include <sys/types.h>
+#include <pthread.h>
+#include <assert.h>
+
 class Map {
 public:
+  virtual ~Map() {}
+
+  virtual void store(const std::string & key, const std::string & value) = 0;
+  virtual bool find(const std::string & key, std::string & value) = 0;
+  virtual bool member(const std::string & key) = 0;
+  virtual size_t size() = 0;
+  virtual unsigned totalValueLength() = 0;
 };
+
+typedef std::tr1::unordered_map<std::string, std::string> hashmap_t;
+
+class MutexMap : public Map {
+public:
+  MutexMap() {
+    int ret = pthread_mutex_init(&mtx_, NULL);
+    assert(ret == 0);
+  }
+
+  ~MutexMap() {
+    int ret = pthread_mutex_destroy(&mtx_);
+    assert(ret == 0);
+  }
+
+  virtual void store(const std::string & key, const std::string & value) {
+    pthread_mutex_lock(&mtx_);
+    impl_[key] = value;
+    pthread_mutex_unlock(&mtx_);
+  }
+
+  virtual bool find(const std::string & key, std::string & value) {
+    pthread_mutex_lock(&mtx_);
+    hashmap_t::const_iterator it = impl_.find(key);
+    
+    bool exists;
+    if(it == impl_.end()) {
+      exists = false;
+    } else {
+      value = it->second;
+      exists =  true;
+    }
+    pthread_mutex_unlock(&mtx_);
+
+    return exists;
+  }
+  
+  virtual bool member(const std::string & key) {
+    pthread_mutex_lock(&mtx_);
+    bool exists = impl_.find(key) != impl_.end();
+    pthread_mutex_unlock(&mtx_);
+
+    return exists;
+  }
+
+  virtual size_t size() { 
+    pthread_mutex_lock(&mtx_);
+    size_t size = impl_.size(); 
+    pthread_mutex_unlock(&mtx_);
+    return size;
+  }
+
+  virtual unsigned totalValueLength() {
+    unsigned total = 0;
+
+    pthread_mutex_lock(&mtx_);
+    hashmap_t::const_iterator it = impl_.begin();
+    for(; it != impl_.end(); ++it) {
+      total += it->second.size();
+    }
+    pthread_mutex_unlock(&mtx_);
+    
+    return total;
+  }
+  
+private:
+  hashmap_t impl_;
+  pthread_mutex_t mtx_;
+};
+
 
 #endif
--- src/bin/mt-bench.cc
+++ src/bin/mt-bench.cc
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 #include "nano_timer.hh"
 #include "map.hh"
 
@@ -15,19 +17,28 @@ enum OP {
   OP_SUM
 };
 
+enum MAP_TYPE {
+  MAP_TYPE_MUTEX,
+  MAP_TYPE_RWLOCK,
+  MAP_TYPE_PERSISTENT
+};
+
 typedef std::vector<std::string> KeyList;
 typedef std::vector<OP> OpList;
 
 struct Param {
   Param(char ** argv)
-    : thread_num(atoi(argv[1])),
-      init_entry_num(atoi(argv[2])),
-      write_op_num(atoi(argv[3])),
-      read_op_num(atoi(argv[4])),
-      sum_op_num(atoi(argv[5]))
+    : map_type(strcmp(argv[1], "mutex") == 0 ? MAP_TYPE_MUTEX : 
+               strcmp(argv[1], "rwlock") == 0 ? MAP_TYPE_RWLOCK : MAP_TYPE_PERSISTENT),
+      thread_num(atoi(argv[2])),
+      init_entry_num(atoi(argv[3])),
+      write_op_num(atoi(argv[4])),
+      read_op_num(atoi(argv[5])),
+      sum_op_num(atoi(argv[6]))
   {
   }
   
+  const MAP_TYPE map_type;
   const unsigned thread_num;
   const unsigned init_entry_num;
   const unsigned write_op_num;
@@ -35,19 +46,30 @@ struct Param {
   const unsigned sum_op_num;
 };
 
+struct Result {
+  Result() : found_count(0), sum_acc(0) {}
+
+  unsigned found_count;
+  unsigned sum_acc;
+};
+
 struct ThreadData {
-  ThreadData(unsigned thread_index, Map & map, const KeyList & keys, const OpList & ops)
+  ThreadData() : thread_index(0), param(NULL), map(NULL), keys(NULL), ops(NULL) {}
+  ThreadData(unsigned thread_index, Param * param, Map * map, KeyList * keys, OpList * ops)
     : thread_index(thread_index),
+      param(param),
       map(map),
       keys(keys),
       ops(ops)
   {
   }
 
-  const unsigned  thread_index;
-  Map           & map;
-  const KeyList & keys;
-  const OpList  & ops;
+  unsigned  thread_index;
+  Param   * param;
+  Map     * map;
+  KeyList * keys;
+  OpList  * ops;
+  Result    rlt;
 };
 
 void gen_input_data(KeyList & init_keys, KeyList & keys, OpList & ops, const Param & param) {
@@ -86,16 +108,50 @@ void gen_input_data(KeyList & init_keys, KeyList & keys, OpList & ops, const Par
   }
 }
 
+void * do_bench(void * data) {
+  ThreadData & td = *static_cast<ThreadData*>(data);
+  Map     & map  = *td.map;
+  KeyList & keys = *td.keys;
+  OpList  & ops  = *td.ops;
+  Result & rlt = td.rlt;
+
+  const int beg_i = keys.size() * static_cast<double>(td.thread_index)   / td.param->thread_num;
+  const int end_i = keys.size() * static_cast<double>(td.thread_index+1) / td.param->thread_num;
+
+  for(int i=beg_i; i < end_i; i++) {
+    const std::string & key = keys[i];
+    const OP op = ops[i];
+    
+    switch(op) {
+    case OP_READ:
+      if(map.member(key)) {
+        rlt.found_count++;
+      }
+      break;
+      
+    case OP_WRITE:
+      map.store(key, key);
+      break;
+
+    case OP_SUM:
+      rlt.sum_acc += map.totalValueLength();
+      break;
+    }
+  }
+  
+  return &rlt;
+}
 
 int main(int argc, char ** argv) {
-  if(argc != 6) {
-    std::cerr << "Usage: mt-bench THREAD_NUM INIT_ENTRY_NUM WRITE_OP_NUM READ_OP_NUM SUM_OP_NUM" << std::endl;
+  if(argc != 7) {
+    std::cerr << "Usage: mt-bench MAP_TYPE(mutex|rwlock|persistent) THREAD_NUM INIT_ENTRY_NUM WRITE_OP_NUM READ_OP_NUM SUM_OP_NUM" << std::endl;
     return 1;
   }
   
-  const Param param(argv);
+  Param param(argv);
 
   std::cout << "[input parameter]" << std::endl
+            << "  map_type      : " << argv[1] << "(" << param.map_type << ")" << std::endl
             << "  thread_num    : " << param.thread_num << std::endl
             << "  init_entry_num: " << param.init_entry_num << std::endl
             << "  write_op_num  : " << param.write_op_num << std::endl
@@ -107,14 +163,56 @@ int main(int argc, char ** argv) {
   KeyList keys;
   OpList ops;
 
-  NanoTimer gen_input_time;
-  gen_input_data(init_keys, keys, ops, param);
-  std::cout << "[generate input data]" << std::endl
-            << "  init_keys: " << init_keys.size() << std::endl
-            << "  keys     : " << keys.size() << std::endl
-            << "  ops      : " << ops.size() << std::endl
-            << "  elapsed  : " << gen_input_time.elapsed_sec() << " sec" << std::endl
-            << std::endl;
+  Map * map = NULL;
+  switch (param.map_type) {
+  case MAP_TYPE_MUTEX:      map = new MutexMap(); break;
+  case MAP_TYPE_RWLOCK:     map = NULL; break;
+  case MAP_TYPE_PERSISTENT: map = NULL; break;
+  }
 
+  {
+    NanoTimer gen_input_time;
+    gen_input_data(init_keys, keys, ops, param);
+    std::cout << "[generate input data]" << std::endl
+              << "  init_keys: " << init_keys.size() << std::endl
+              << "  keys     : " << keys.size() << std::endl
+              << "  ops      : " << ops.size() << std::endl
+              << "  elapsed  : " << gen_input_time.elapsed_sec() << " sec" << std::endl
+              << std::endl;
+  }
+
+  for(size_t i=0; i < init_keys.size(); i++) {
+    map->store(init_keys[i], init_keys[i]);
+  }
+
+  {
+    NanoTimer bench_time;
+
+    std::vector<pthread_t> threads(param.thread_num);
+    std::vector<ThreadData> tdatas(param.thread_num);
+
+    for(unsigned i=0; i < param.thread_num; i++) {
+      tdatas[i] = ThreadData(i, &param, map, &keys, &ops);
+      int ret = pthread_create(&threads[i], NULL, do_bench, &tdatas[i]);
+      assert(ret == 0);
+    }
+    
+    Result rlt;
+    for(unsigned i=0; i < param.thread_num; i++) {
+      Result * tmp;
+      pthread_join(threads[i], reinterpret_cast<void**>(&tmp));
+      rlt.found_count += tmp->found_count;
+      rlt.sum_acc += tmp->sum_acc;
+    }
+
+    std::cout << "[bench result]" << std::endl
+              << "  founds : " << rlt.found_count << std::endl
+              << "  sum_acc: " << rlt.sum_acc << std::endl
+              << "  keys   : " << map->size() << std::endl
+              << "  elapsed: " << bench_time.elapsed_sec() << " sec" << std::endl
+              << std::endl;
+
+    delete map;
+  }
   return 0;
 }
